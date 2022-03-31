3082d6802c8d80dfa03e97a327ad90373aa120b1
Eugenia <eugeniasaf@gmail.com>
2022-03-31 16:39:09 +0200

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line strict
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
